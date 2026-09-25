6522550922f9e867cae81f9ec7a5b4f288cbb6b4
albertoagl777 <albertoaglagl@gmail.com>
2026-09-25 11:55:47 +0200

    Add files via upload

--- sw.js
+++ sw.js
@@ -6,7 +6,7 @@
    VERSION. Eso es lo que hace que el móvil se descargue la versión nueva.
    =========================================================================== */
 
-const VERSION = "v3";
+const VERSION = "v4";
 const CACHE = "misviajes-" + VERSION;
 
 const ARCHIVOS = [
